28444e458b723e64c11c90e0104791f07e4a7091
Kishwar <kishwar.shafin@gmail.com>
2018-03-31 02:22:05 -0700

    maybe we need one more flag

--- train_candidate_finder.py
+++ train_candidate_finder.py
@@ -267,7 +267,7 @@ def genome_level_parallelization(bam_file, ref_file, vcf_file, confident_tree, o
     """
     # chr_list = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
     #             "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19"]
-    chr_list = ["19"]
+    chr_list = ["chr19"]
     program_start_time = time.time()
 
     # chr_list = ["chr19"]
