8f6d31e8c3542a18e0e5f0c5cbf7b4a689a91419
Kishwar <kishwar.shafin@gmail.com>
2018-04-07 13:15:08 -0700

    the weight decay is off

--- train.py
+++ train.py
@@ -87,7 +87,7 @@ def train(train_file, validation_file, batch_size, epoch_limit, file_name, gpu_m
     sys.stderr.write(TextColor.PURPLE + 'Data loading finished\n' + TextColor.END)\
 
     model = ModelHandler.get_new_model(gpu_mode)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
     if gpu_mode:
         model = model.cuda()
