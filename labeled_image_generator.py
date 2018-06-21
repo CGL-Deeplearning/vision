c63d1b5d0937f29eef47ed02e085e1eafac39537
geezlooueez <ldcabansay@gmail.com>
2018-06-20 17:11:34 -0700

    set image size 30x100

--- labeled_image_generator.py
+++ labeled_image_generator.py
@@ -202,7 +202,7 @@ class View:
         """
 
         # declare the size of the image
-        image_height, image_width = 300, 300
+        image_height, image_width = 30, 100
         if len(candidate_list) == 0:
             return
 
