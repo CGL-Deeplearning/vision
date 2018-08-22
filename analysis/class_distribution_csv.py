e5dda1e09a9cfdd9440b8fc75a61cbfa5b6a17cc
Kishwar <kishwar.shafin@gmail.com>
2018-08-22 14:26:45 -0700

    class distribution fixed :fire:

--- analysis/class_distribution_csv.py
+++ analysis/class_distribution_csv.py
@@ -20,7 +20,7 @@ with open(file_name, "r") as ins:
         if not line:
             continue
         line = line.split(',')
-        gt = line[2]
+        gt = line[2].split('\t')[-1]
         dictionary[gt] += 1
 
 
