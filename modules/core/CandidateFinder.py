bd6f022fae0a5c414fff88f4a85e612ec41df91f
geezlooueez <ldcabansay@gmail.com>
2018-08-27 13:13:08 -0700

    bug fix, if -> elif

--- modules/core/CandidateFinder.py
+++ modules/core/CandidateFinder.py
@@ -398,7 +398,7 @@ class CandidateFinder:
             frequency = round(count / self.coverage[position], 3) if self.coverage[position] else 0
             if allele_type == INSERT_ALLELE or allele_type == DELETE_ALLELE:
                 filtered_list.append((allele, count, frequency))
-            if count > MIN_MISMATCH_THRESHOLD and frequency * 100 > MIN_MISMATCH_PERCENT_THRESHOLD\
+            elif count > MIN_MISMATCH_THRESHOLD and frequency * 100 > MIN_MISMATCH_PERCENT_THRESHOLD\
                     and coverage > MIN_COVERAGE_THRESHOLD:
                 filtered_list.append((allele, count, frequency))
         return filtered_list
