81c3a5e5c0743d6691e90317c0aba930477fa25d
geezlooueez <ldcabansay@gmail.com>
2018-08-28 10:44:59 -0700

    remove all filter thresholds

--- modules/core/CandidateFinder.py
+++ modules/core/CandidateFinder.py
@@ -396,11 +396,11 @@ class CandidateFinder:
             allele_sequence, allele_type = allele
             coverage = self.coverage[position] if self.coverage[position] else 0
             frequency = round(count / self.coverage[position], 3) if self.coverage[position] else 0
-            if allele_type == INSERT_ALLELE or allele_type == DELETE_ALLELE:
-                filtered_list.append((allele, count, frequency))
-            elif count > MIN_MISMATCH_THRESHOLD and frequency * 100 > MIN_MISMATCH_PERCENT_THRESHOLD\
-                    and coverage > MIN_COVERAGE_THRESHOLD:
-                filtered_list.append((allele, count, frequency))
+            #if allele_type == INSERT_ALLELE or allele_type == DELETE_ALLELE:
+            #    filtered_list.append((allele, count, frequency))
+            #elif count > MIN_MISMATCH_THRESHOLD and frequency * 100 > MIN_MISMATCH_PERCENT_THRESHOLD\
+            #        and coverage > MIN_COVERAGE_THRESHOLD:
+            filtered_list.append((allele, count, frequency))
         return filtered_list
 
     def _get_record(self, pos, alt1, alt2, ref, ref_count):
