ea078adb6b2620eef71210104e2b404c4bb51343
Kishwar <kishwar.shafin@gmail.com>
2018-10-27 11:33:49 -0700

    pretrained model loading enabled :fire:

--- modules/models/ModelHandler.py
+++ modules/models/ModelHandler.py
@@ -1,6 +1,7 @@
 import torch
-from modules.models.inception import inception_v3
+# from modules.models.inception import inception_v3
 from modules.models.resnet_pytorch import resnet18
+from modules.models.inception_pytorch import inception_v3
 
 class ModelHandler:
     @staticmethod
@@ -9,7 +10,7 @@ class ModelHandler:
 
     @staticmethod
     def get_new_model():
-        model = inception_v3()
+        model = inception_v3(pretrained=True)
         return model
 
     @staticmethod
